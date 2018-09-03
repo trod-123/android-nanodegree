6a90436d5782e5f499fe799c9b36c5c53a8121ea
Teddy Rodriguez <cia.123trod@gmail.com>
2018-09-02 23:54:39 -0700

    ExpiryTracker. Project set-up and initial commit
    - Import all necessary libraries
    - Set the debug and release build configuration
    - Set the free and paid product flavors
    - Enable MultiDex support

---
+++ ExpiryTracker/app/src/main/java/com/zn/expirytracker/ExpiryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.zn.expirytracker;
+
+import android.support.multidex.MultiDexApplication;
+
+public class ExpiryTrackerApplication extends MultiDexApplication {
+
+//    @Override
+//    protected void attachBaseContext(Context base) {
+//        super.attachBaseContext(base);
+//        // If we can't extend MultiDexApplication class, run this instead
+//        MultiDex.install(this);
+//    }
+}
