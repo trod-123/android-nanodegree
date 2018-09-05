861d813fe4bf15314ab70e0fc948258bf5a206fb
trod-123 <cia.123trod@gmail.com>
2018-09-04 23:00:08 -0700

    ExpiryTracker. Add Timber
    Signed-off-by: Teddy Rodriguez <cia.123trod@gmail.com>

--- ExpiryTracker/app/src/main/java/com/zn/expirytracker/ExpiryTrackerApplication.java
+++ ExpiryTracker/app/src/main/java/com/zn/expirytracker/ExpiryTrackerApplication.java
@@ -2,6 +2,8 @@ package com.zn.expirytracker;
 
 import android.support.multidex.MultiDexApplication;
 
+import timber.log.Timber;
+
 public class ExpiryTrackerApplication extends MultiDexApplication {
 
 //    @Override
@@ -10,4 +12,17 @@ public class ExpiryTrackerApplication extends MultiDexApplication {
 //        // If we can't extend MultiDexApplication class, run this instead
 //        MultiDex.install(this);
 //    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            // TODO: Initialize a crash reporting tree
+            // https://github.com/JakeWharton/timber/blob/master/sample/src/main/java/com/example/timber/ExampleApp.java
+        }
+    }
 }
