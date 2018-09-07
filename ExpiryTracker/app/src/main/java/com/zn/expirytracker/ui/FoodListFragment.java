c1e99ee3dac2995d5e0cb246c1664b56ef1c021f
Teddy Rodriguez <cia.123trod@gmail.com>
2018-09-06 18:49:00 -0700

    ExpiryTracker. Add test data to ListFragment
    Signed-off-by: Teddy Rodriguez <cia.123trod@gmail.com>

--- ExpiryTracker/app/src/main/java/com/zn/expirytracker/ui/FoodListFragment.java
+++ ExpiryTracker/app/src/main/java/com/zn/expirytracker/ui/FoodListFragment.java
@@ -1,17 +1,30 @@
 package com.zn.expirytracker.ui;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zn.expirytracker.R;
+import com.zn.expirytracker.data.TestDataGen;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
 public class FoodListFragment extends Fragment {
+
+    @BindView(R.id.rv_food_list)
+    RecyclerView mRvFoodList;
+
+    private Activity mHostActivity;
+
+    private FoodListAdapter mListAdapter;
+    private TestDataGen mDataGenerator;
 
     public FoodListFragment() {
         // Required empty public constructor
@@ -19,6 +32,7 @@ public class FoodListFragment extends Fragment {
 
     /**
      * TODO: Temp, for if we need to pass any arguments
+     *
      * @return
      */
     public static FoodListFragment newInstance() {
@@ -30,6 +44,8 @@ public class FoodListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mHostActivity = getActivity();
+        mDataGenerator = TestDataGen.getInstance();
     }
 
     @Override
@@ -40,6 +56,19 @@ public class FoodListFragment extends Fragment {
         Timber.tag(FoodListFragment.class.getSimpleName());
         ButterKnife.bind(this, rootView);
 
+        setupRecyclerView();
+
         return rootView;
+    }
+
+    private void setupRecyclerView() {
+        mRvFoodList.setLayoutManager(new LinearLayoutManager(mHostActivity,
+                LinearLayoutManager.VERTICAL, false));
+        mRvFoodList.setHasFixedSize(true);
+        // TODO: Implement under LiveData
+        mListAdapter = new FoodListAdapter(mDataGenerator.getFoodNames(),
+                mDataGenerator.getExpiryDates(), mDataGenerator.getCounts(),
+                mDataGenerator.getLocs());
+        mRvFoodList.setAdapter(mListAdapter);
     }
 }
