e5ee93975be28619ed21f0339bd4df54762551da
trod-123 <cia.123trod@gmail.com>
2018-09-25 03:29:01 -0700

    ExpiryTracker. Set signed out Widget layout
    Signed-off-by: Teddy Rodriguez <cia.123trod@gmail.com>

--- ExpiryTracker/app/src/main/java/com/zn/expirytracker/widget/FoodWidget.java
+++ ExpiryTracker/app/src/main/java/com/zn/expirytracker/widget/FoodWidget.java
@@ -15,6 +15,8 @@ import com.zn.expirytracker.R;
 import com.zn.expirytracker.settings.SettingsActivity;
 import com.zn.expirytracker.ui.DetailActivity;
 import com.zn.expirytracker.ui.MainActivity;
+import com.zn.expirytracker.ui.SignInActivity;
+import com.zn.expirytracker.utils.AuthToolbox;
 import com.zn.expirytracker.utils.DataToolbox;
 import com.zn.expirytracker.utils.Toolbox;
 
@@ -55,57 +57,73 @@ public class FoodWidget extends AppWidgetProvider {
                                 int appWidgetId, boolean hideProgressBar) {
         Timber.e("In updateAppWidget(): appWidgetId=%s", appWidgetId);
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.food_widget);
+        RemoteViews views;
 
-        if (!hideProgressBar) {
-            views.setViewVisibility(R.id.pb_widget, View.VISIBLE);
-            int sRandomNumber = (new Random()).nextInt();
+        if (AuthToolbox.isSignedIn()) {
+            // Set the regular widget
+            views = new RemoteViews(context.getPackageName(), R.layout.food_widget);
+            if (!hideProgressBar) {
+                views.setViewVisibility(R.id.pb_widget, View.VISIBLE);
+                int sRandomNumber = (new Random()).nextInt();
 
-            // get the entry corresponding to the set widget setting value and set as header
-            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-            int spValue = Integer.parseInt(sp.getString(context.getString(R.string.pref_widget_num_days_key), "3"));
-            String spEntry = context.getResources()
-                    .getStringArray(R.array.pref_expire_num_days_entries)[spValue].toLowerCase();
-            String header = context.getString(R.string.expiry_msg_generic_pp, spEntry);
-            views.setTextViewText(R.id.tv_widget_header, header);
+                // get the entry corresponding to the set widget setting value and set as header
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+                int spValue = Integer.parseInt(sp.getString(context.getString(R.string.pref_widget_num_days_key), "3"));
+                String spEntry = context.getResources()
+                        .getStringArray(R.array.pref_expire_num_days_entries)[spValue].toLowerCase();
+                String header = context.getString(R.string.expiry_msg_generic_pp, spEntry);
+                views.setTextViewText(R.id.tv_widget_header, header);
 
-            // set the header click listener
-            Intent mainIntent = new Intent(context, MainActivity.class);
-            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent mainPendingIntent = PendingIntent.getActivity(
-                    context, appWidgetId, mainIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            views.setOnClickPendingIntent(R.id.layout_widget_header, mainPendingIntent);
+                // set the header click listener
+                Intent mainIntent = new Intent(context, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent mainPendingIntent = PendingIntent.getActivity(
+                        context, appWidgetId, mainIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                views.setOnClickPendingIntent(R.id.layout_widget_header, mainPendingIntent);
 
-            // set the settings button click listener
-            Intent settingsIntent = new Intent(context, SettingsActivity.class);
-            settingsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent settingsPendingIntent = PendingIntent.getActivity(
-                    context, appWidgetId, settingsIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            views.setOnClickPendingIntent(R.id.btn_widget_settings, settingsPendingIntent);
+                // set the settings button click listener
+                Intent settingsIntent = new Intent(context, SettingsActivity.class);
+                settingsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent settingsPendingIntent = PendingIntent.getActivity(
+                        context, appWidgetId, settingsIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                views.setOnClickPendingIntent(R.id.btn_widget_settings, settingsPendingIntent);
 
-            // set the list adapter
-            Intent listViewIntent = new Intent(context, FoodListRemoteViewsService.class);
-            // set data to appWidgetId to ensure pending intent is unique per appWidgetId
-            // pass a random id to ensure RemoteViewsFactory returns a new factory when a request to
-            // update is sent
-            // https://stackoverflow.com/questions/14486888/android-appwidget-listview-not-updating
-            listViewIntent.setData(Uri.fromParts(AppWidgetManager.EXTRA_APPWIDGET_ID,
-                    String.valueOf(appWidgetId + sRandomNumber), null));
-            views.setRemoteAdapter(R.id.lv_widget, listViewIntent);
+                // set the list adapter
+                Intent listViewIntent = new Intent(context, FoodListRemoteViewsService.class);
+                // set data to appWidgetId to ensure pending intent is unique per appWidgetId
+                // pass a random id to ensure RemoteViewsFactory returns a new factory when a request to
+                // update is sent
+                // https://stackoverflow.com/questions/14486888/android-appwidget-listview-not-updating
+                listViewIntent.setData(Uri.fromParts(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                        String.valueOf(appWidgetId + sRandomNumber), null));
+                views.setRemoteAdapter(R.id.lv_widget, listViewIntent);
 
-            // set the pending intent template for individual item clicks
-            Intent detailIntent = new Intent(context, DetailActivity.class);
-            PendingIntent detailPendingIntent = PendingIntent.getActivity(
-                    context, 0, detailIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            views.setPendingIntentTemplate(R.id.lv_widget, detailPendingIntent);
+                // set the pending intent template for individual item clicks
+                Intent detailIntent = new Intent(context, DetailActivity.class);
+                PendingIntent detailPendingIntent = PendingIntent.getActivity(
+                        context, 0, detailIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                views.setPendingIntentTemplate(R.id.lv_widget, detailPendingIntent);
 
-            // hides the empty view until needed
-            views.setEmptyView(R.id.lv_widget, R.id.tv_widget_empty);
+                // hides the empty view until needed
+                views.setEmptyView(R.id.lv_widget, R.id.tv_widget_empty);
+                views.setImageViewResource(R.id.iv_widget_empty_animal,
+                        DataToolbox.getRandomAnimalDrawableId());
+            } else {
+                // We're just hiding the progress bar; all views are already updated
+                views.setViewVisibility(R.id.pb_widget, View.GONE);
+            }
+        } else {
+            // User is not signed in. Get alternate layout
+            views = new RemoteViews(context.getPackageName(), R.layout.food_widget_signed_out);
+            // set the sign-in pending intent
+            Intent signInIntent = new Intent(context, SignInActivity.class);
+            signInIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent signInPendingIntent = PendingIntent.getActivity(
+                    context, appWidgetId, signInIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setOnClickPendingIntent(R.id.layout_widget_signed_out, signInPendingIntent);
+
             views.setImageViewResource(R.id.iv_widget_empty_animal,
                     DataToolbox.getRandomAnimalDrawableId());
-        } else {
-            // We're just hiding the progress bar; all views are already updated
-            views.setViewVisibility(R.id.pb_widget, View.GONE);
         }
 
         // Instruct the widget manager to update the widget
