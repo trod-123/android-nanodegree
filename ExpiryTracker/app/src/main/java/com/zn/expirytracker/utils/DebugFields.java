b0d8eb35e05b12a3351f5530edcf5bd290ea896a
trod-123 <cia.123trod@gmail.com>
2018-10-04 23:58:49 -0700

    ExpiryTracker. Fix release compile error due to missing default dimens
    ^ Error: Caused by: org.gradle.api.GradleException: Lint found fatal errors while assembling a release target
    
    Signed-off-by: Teddy Rodriguez <cia.123trod@gmail.com>

--- ExpiryTracker/app/src/main/java/com/zn/expirytracker/utils/DebugFields.java
+++ ExpiryTracker/app/src/main/java/com/zn/expirytracker/utils/DebugFields.java
@@ -8,7 +8,7 @@ public class DebugFields {
     /**
      * For demoing. {@code true} enables the Demo test account to show in the Sign-in activity
      */
-    public static final boolean DEMO_TEST_ACCOUNT_ACTIVATED = true;
+    public static final boolean DEMO_TEST_ACCOUNT_ACTIVATED = false;
     public static final String DEMO_TEST_ACCOUNT_ID = "GWdCRZ7KL5hda4KgB9kngU1jiKL2";
     public static final String DEMO_TEST_EMAIL = "demo@zn.io";
     public static final String DEMO_TEST_PASSWORD = "7H3D066035W00F4ND7H3C0W6035M00!!!";
